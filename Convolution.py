a06950b989a1860338203c53e968d2cd081642c1
Vladyslav Zavedenski <zavedenski@gmail.com>
2018-01-07 11:36:41 +0200

    Last version which works, need to add filter and multiply array by array.

--- Convolution.py
+++ Convolution.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from skimage import img_as_ubyte
 import skimage.measure as msr
 from PIL import Image
 import numpy as np
@@ -12,7 +13,7 @@ imageArrayList = imageArray.tolist()
 #---Open-Origin-Image--END-------------------------------------------
 
 #---Open-Filter-Image------------------------------------------------
-filterFile = Image.open("Filters/10x10.jpg")
+filterFile = Image.open("Filters/line3.jpg")
 filterFile = filterFile.convert('L')
 filterArray = np.array(filterFile)
 filterArrayList = filterArray.tolist()
@@ -37,46 +38,48 @@ while True: #Rows
             newImage.append(imageArray[xAsix:xAsix+20, yAsix:yAsix+20])
             yAsix += 1
         else:
-            yAsix = 0;
-            break
-
+            yAsix = 0;  break
     if xAsix < xLen-20:
         xAsix += 1
-    else:
-        break
+    else:   break
 
 print("NewImage[0]: ", newImage[0])
 print("np.mean(newImage[0]:) ", np.mean(newImage[0]))
 print("NewImageLen: ", len(newImage))
 
+#---newImage[78961[20[20]]]------------------------------------------
 for i in range(len(newImage)):
     newImage[i] = np.mean(newImage[i])
+#---newImage[78961[float64]]--End------------------------------------
+
+#---newImage[78961[float64]]-----------------------------------------
+Buff = []
+for i in range(0, 78961, 281):
+    Buff.append(newImage[i:i+281])
+#---Buff[281[281[float64]]]--End-------------------------------------
 
 
 xero = np.empty((281,281))             #f(300-20)/1+1
-
-dead = []
-o = 0
-while True:
-    if o < 78961:
-        dead.append(newImage[o:o+281])
-        o += 281
-    else:
-        break
-
-#print(dead[0])
-print(len(dead[0]))
-
-
+print("xero: %d, xero[0]: %d" %(len(xero), len(xero[0])))
+print("Buff: %d, Buff[0]: %d" %(len(Buff), len(Buff[0])))
+if len(xero) == len(Buff) and len(xero[0]) == len(Buff[0]) and type(xero[0][0]) == type(Buff[0][0]):
+    print("Variables are same!")
+else:
+    print("Variables are difference!")
 
 x, y = 0, 0
 while True:
     while True:
         if y < 281:
-            xero[x, y] = np.average(dead[x][y])
+            xero[x][y] = int(np.mean(Buff[x][y]))
+            print('[x][y] = [%d][%d] ==> %d' %(x,y, Buff[x][y]))
             y += 1
         else:
             y = 0;  break
-    if x < 281:
+    if x < 281-1:
         x +=1
     else:   break
+
+xeroImg = Image.fromarray(xero)
+xeroImg  = xeroImg.convert('L')
+xeroImg.save("Result.jpg")
